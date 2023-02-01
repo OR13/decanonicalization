8f792ddaef05dccaa0e08f43befc0f853cdf2e4c
Orie Steele <orie@transmute.industries>
2023-02-01 07:29:17 -0600

    :rainbow::rocket:

--- generate-times.js
+++ generate-times.js
@@ -7,7 +7,8 @@ import vendor1 from './vendor-1.js'
 import { generateSeries } from './common.js'
 
 (async ()=> {
-  const count = 16
+  // 16 takes too long - https://github.com/OR13/decanonicalization/actions/runs/4058366235
+  const count = 10
   const increment = 1;
   const seriesA = await generateSeries(vendor0, 'Data Integrity', count, increment)
   const seriesB = await generateSeries(vendor1, 'JSON Web Signature', count, increment)
